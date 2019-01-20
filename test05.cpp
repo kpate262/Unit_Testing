0cc0a3cdf0347048065496e78805cbea45462856
Kisan Patel <kpate262@uic.edu>
2019-01-20 21:39:12 +0000

    testing test05

--- test05.cpp
+++ test05.cpp
@@ -28,30 +28,41 @@ using namespace std;
 
 TEST_CASE( "Test 05", "[Project01]" ) 
 {
-  /*Course C("CS", "Professional Seminar", 499, 01, "Hummel", 
-           A-F 0, 0, 0, 0, 0, 
-           I,S,U,W,NR 2, 88, 0, 0, 1);
-           */
-  Course C = ParseCourse("CS,499,01,Professional Seminar,0,0,0,0,0,2,1, 88,0,0,Hummel");
+  Course C("CS", "Professional Seminar", 499, 01, "Hummel", 
+           /*A-F*/ 1, 2, 3, 4, 5, 
+           /*I,S,U,W,NR*/ 2, 88, 0, 1, 1);
+  //Course C = ParseCourse("CS,499,01,Professional Seminar,0,0,0,0,0,2,1, 88,0,0,Hummel");
   REQUIRE(C.Dept == "CS");
   REQUIRE(C.Title == "Professional Seminar");
   REQUIRE(C.Number == 499);
   REQUIRE(C.Section == 01);
   REQUIRE(C.Instructor == "Hummel");
   
-  REQUIRE(C.NumA == 0);
-  REQUIRE(C.NumB == 0);           
-  REQUIRE(C.NumC == 0);
-  REQUIRE(C.NumD == 0);
-  REQUIRE(C.NumF == 0);
+  REQUIRE(C.NumA == 1);
+  REQUIRE(C.NumB == 2);           
+  REQUIRE(C.NumC == 3);
+  REQUIRE(C.NumD == 4);
+  REQUIRE(C.NumF == 5);
   
   REQUIRE(C.NumI == 2);
   REQUIRE(C.NumS == 88);           
   REQUIRE(C.NumU == 0);
-  REQUIRE(C.NumW == 0);
+  REQUIRE(C.NumW == 1);
   REQUIRE(C.NumNR == 1);
     
-  //GradeStats G = GetGradeDistribution
+  GradeStats G = GetGradeDistribution(C);
+  REQUIRE(G.N == 15);
+  REQUIRE(G.NumA == 1);
+      REQUIRE(G.NumB == 2);
+      REQUIRE(G.NumC == 3);
+      REQUIRE(G.NumD == 4);
+      REQUIRE(G.NumF == 5);
+      REQUIRE(G.PercentA == ((1.0/15)*100.0));
+    REQUIRE(G.PercentB == ((2.0/15)*100.0));
+    REQUIRE(G.PercentC == ((3.0/15)*100.0));
+    REQUIRE(G.PercentD == ((4.0/15)*100.0));
+    REQUIRE(G.PercentF == ((5.0/15)*100.0));
+      
   
   //REQUIRE(C.getGradingType() == Course::Satisfactory);
   //REQUIRE(C.getNumStudents() == )
