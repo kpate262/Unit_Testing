3371d85eaa9862614849ee4beaf2254737630952
Kisan Patel <kpate262@uic.edu>
2019-01-20 22:59:19 +0000

    testing test07

--- test07.cpp
+++ test07.cpp
@@ -26,7 +26,7 @@
 using namespace std;
 
 
-TEST_CASE( "Test 06", "[Project01]" ) 
+TEST_CASE( "Test 07", "[Project01]" ) 
 {
   Dept C("CS");
   C.Courses.push_back(Course("CS", "Professional Seminar", 499, 01, "Hummel", 1, 2, 3, 4, 5, 2, 88, 0, 1, 1));
@@ -34,21 +34,15 @@ TEST_CASE( "Test 06", "[Project01]" )
       C.Courses.push_back(Course("CS", "Professio", 49, 01, "Hummfdfd", 1, 20, 30, 40, 5, 2, 88, 0, 51, 1));
       C.Courses.push_back(Course("CS", "Profes", 500, 01, "melHum", 15, 22, 32, 48, 59, 2, 88, 0, 431, 1));
       C.Courses.push_back(Course("CS", "Profe", 339, 01, "Dale", 12, 24, 33, 49, 56, 2, 88, 0, 6, 1));
-  REQUIRE(C.Courses.size() == 5);
-  GradeStats G = GetGradeDistribution(C);
-  REQUIRE(G.N == 529);
-  REQUIRE(G.NumA == 34);
-      REQUIRE(G.NumB == 109);
-      REQUIRE(G.NumC == 111);
-      REQUIRE(G.NumD == 145);
-      REQUIRE(G.NumF == 130);
-      REQUIRE(G.PercentA == ((34.0/529)*100.0));
-    REQUIRE(G.PercentB == ((109.0/529)*100.0));
-    REQUIRE(G.PercentC == ((111.0/529)*100.0));
-    REQUIRE(G.PercentD == ((145.0/529)*100.0));
-    REQUIRE(G.PercentF == ((130.0/529)*100.0));
+    //W = 1+13+51+431+6 = 502
+    //D+F = 4+5+4+5+5+40+48+59+49+56 = 275
+    //DFW = 777
+    //N = 1031
+  int DFW = 0; int N = 0;
+   REQUIRE(GetDFWRate(C, DFW, N) >= 75.363);
+    REQUIRE(GetDFWRate(C, DFW, N) <= 75.365);
+    REQUIRE(DFW == 777);
+    REQUIRE(N == 1031);
+    
       
-  
-  //REQUIRE(C.getGradingType() == Course::Satisfactory);
-  //REQUIRE(C.getNumStudents() == )
 }
