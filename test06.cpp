7e8fac7fd8f1f826314dce016e1c27500ff2a6de
Kisan Patel <kpate262@uic.edu>
2019-01-21 05:58:06 +0000

    test 06

--- test06.cpp
+++ test06.cpp
@@ -28,38 +28,33 @@ using namespace std;
 
 TEST_CASE( "Test 06", "[Project01]" ) 
 {
-  Dept CS("CS");
-      CS.Courses.push_back(Course("CS", "Professional Seminar", 499, 01, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, -1, 1));
-      CS.Courses.push_back(Course("CS", "Professional Se", 500, 4, "Humm", 5, 41, 13, 4, 5, 2, 88, 0, 13, 1));
-      CS.Courses.push_back(Course("CS", "Professio", 500, 3, "Hummfdfd", 1, 20, 30, 40, 5, 2, 88, 0, 51, 1));
-      CS.Courses.push_back(Course("CS", "Profes", 151, 1, "melHum", 15, 22, 32, 48, 59, 2, 88, 0, 431, 1));
-      CS.Courses.push_back(Course("CS", "Profe", 251, 02, "Dale", 12, 24, 33, 49, 56, 2, 88, 0, 6, 1));
-      CS.Courses.push_back(Course("CS", "Professional Seminar", 499, 01, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, -1, 1));
-        CS.Courses.push_back(Course("CS", "Professional Se", 500, 4, "Humm", 5, 41, 13, 4, 5, 2, 88, 0, 13, 1));
-      CS.Courses.push_back(Course("CS", "Professio", 500, 3, "Hummfdfd", 1, 20, 30, 40, 5, 2, 88, 0, 51, 1));
-      CS.Courses.push_back(Course("CS", "Profes", 151, 1, "melHum", 15, 22, 32, 48, 59, 2, 88, 0, 431, 1));
-      CS.Courses.push_back(Course("CS", "Profe", 251, 02, "Dale", 12, 24, 33, 49, 56, 2, 88, 0, 6, 1));
-    CS.Courses.push_back(Course("CS", "Professional Seminar", 499, 01, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, -1, 1));
-    CS.Courses.push_back(Course("CS", "Professional Se", 500, 4, "Humm", 5, 41, 13, 4, 5, 2, 88, 0, 13, 1));
-      CS.Courses.push_back(Course("CS", "Professio", 500, 3, "Hummfdfd", 1, 20, 30, 40, 5, 2, 88, 0, 51, 1));
-      CS.Courses.push_back(Course("CS", "Profes", 151, 1, "melHum", 15, 22, 32, 48, 59, 2, 88, 0, 431, 1));
-      CS.Courses.push_back(Course("CS", "Profe", 251, 02, "Dale", 12, 24, 33, 49, 56, 2, 88, 0, 6, 1));
-  
-    
-  GradeStats G = GetGradeDistribution(CS);
-  REQUIRE(G.N == 1542);
-  REQUIRE(G.NumA == 99);
-      REQUIRE(G.NumB == 321);
-      REQUIRE(G.NumC == 324);
-      REQUIRE(G.NumD == 423);
-      REQUIRE(G.NumF == 375);
-      REQUIRE(G.PercentA == ((99.0/1542)*100.0));
-    REQUIRE(G.PercentB == ((321.0/1542)*100.0));
-    REQUIRE(G.PercentC == ((324.0/1542)*100.0));
-    REQUIRE(G.PercentD == ((423.0/1542)*100.0));
-    REQUIRE(G.PercentF == ((375.0/1542)*100.0));
-
-  
-  //REQUIRE(C.getGradingType() == Course::Satisfactory);
-  //REQUIRE(C.getNumStudents() == )
+  Dept C("CS");
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, 88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -4, -5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", 1, 2, 3, 4, 5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", 1, 2, 3, 4, 5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", 1, 2, 3,4, 5, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", 1, 2, 3, 4, -11, 2, -88, 0, 1, 1));
+    C.Courses.push_back(Course("CS", "Professional Seminar", 377, 02, "Hummel", -1, -2, -3, -400, -5, 2, -88, 0, 1, 1));
+        
+    GradeStats G = GetGradeDistribution(C);
+    REQUIRE(G.N == 45);
+    REQUIRE(G.NumA == 3);
+    REQUIRE(G.NumB == 6);
+      REQUIRE(G.NumC == 9);
+      REQUIRE(G.NumD == 12);
+      REQUIRE(G.NumF == 15);
+      REQUIRE(G.PercentA == (3*1.0/45)*100);
+    REQUIRE(G.PercentB == (6*1.0/45)*100);
+    REQUIRE(G.PercentC == (9*1.0/45)*100);
+    REQUIRE(G.PercentD == (12*1.0/45)*100);
+    REQUIRE(G.PercentF == (15*1.0/45)*100);
+   
 }
